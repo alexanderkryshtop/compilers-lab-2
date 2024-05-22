6df028266fd0a7d19f1dae9554573cf9704746d8
Alexander Kryshtop <kxcompare@gmail.com>
2024-05-22 16:07:02 +0300

    Add remove left recursion

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class Rule:
         self.right = [elem.strip() for elem in right]
 
     def __repr__(self):
-        right_str = " ".join(self.right)
+        right_str = " ".join(self.right) or EPSILON
         return f"{self.left} -> {right_str}"
 
 
@@ -53,7 +53,7 @@ class Grammar:
         text = text.strip()
 
         header, *body = text.split("\n")
-        non_term_str, terminal_str, axiom = header.split(" ")
+        non_term_str, terminal_str, start_symbol = header.split(" ")
         non_terms = non_term_str[1:-1].split(",")
         terminals = terminal_str[1:-1].split(",")
 
@@ -71,26 +71,75 @@ class Grammar:
                 if right_symbols[0] == EPSILON:
                     right_symbols = []
                 rules.append(Rule(left, right_symbols))
-        return Grammar(non_terms, terminals, rules, axiom)
+        return Grammar(non_terms, terminals, rules, start_symbol)
 
 
-# grammar = Grammar.parse_grammar(
-#     """
-#     [A,B,C,D] [a,b,c] A
-#     A -> A a | a | B
-#     B -> C b
-#     C -> B c
-#     D -> A
-#     """
-# )
+def remove_left_recursion(grammar: Grammar):
+    new_rules: list[Rule] = grammar.rules.copy()
+    non_terminals = grammar.non_terms.copy()
+
+    for i, current_non_terminal in enumerate(grammar.non_terms):
+        for previous_non_terminal in grammar.non_terms[:i]:
+            left_recursive_rules = filter(
+                lambda r: r.left == current_non_terminal and r.right and r.right[0] == previous_non_terminal, new_rules
+            )
+
+            for rule in left_recursive_rules:
+                new_rules.remove(rule)
+                previous_rules = filter(lambda r: r.left == previous_non_terminal, new_rules)
+
+                for previous_rule in previous_rules:
+                    new_rules.append(Rule(current_non_terminal, previous_rule.right + rule.right[1:]))
+
+        current_rules = list(filter(lambda r: r.left == current_non_terminal, new_rules))
+        has_immediate_left_recursion = any(
+            rule.right and rule.right[0] == current_non_terminal for rule in current_rules
+        )
+
+        if has_immediate_left_recursion:
+            new_non_terminal = current_non_terminal + "'"
+            non_terminals.append(new_non_terminal)
+
+            for rule in current_rules:
+                new_rules.remove(rule)
+
+                if rule.right and rule.right[0] == current_non_terminal:
+                    new_rules += [
+                        Rule(new_non_terminal, rule.right[1:]),
+                        Rule(new_non_terminal, rule.right[1:] + [new_non_terminal])
+                    ]
+                else:
+                    new_rules += [
+                        Rule(rule.left, rule.right.copy()),
+                        Rule(rule.left, rule.right + [new_non_terminal])
+                    ]
+
+    return Grammar(non_terminals, grammar.terms.copy(), new_rules, grammar.start_symbol)
+
+
 grammar = Grammar.parse_grammar(
     """
-    [A,B,C,S] [a,b,c] S
-    A -> a A | ε
-    B -> b B | ε
-    C -> c C | ε
-    S -> A B C
+    [A,B,C,D] [a,b,c] A
+    A -> A a | a | B
+    B -> C b
+    C -> B c
+    D -> A
     """
 )
 # display(grammar.show())
-x = grammar.print()
+# grammar = Grammar.parse_grammar(
+#     """
+#     [A,B,C,S] [a,b,c] S
+#     A -> a A | ε
+#     B -> b B | ε
+#     C -> c C | ε
+#     S -> A B C
+#     """
+# )
+# display(grammar.show())
+grammar.print()
+
+print()
+
+g2 = remove_left_recursion(grammar)
+g2.print()
